b89890e9c6a158206ef44c16f4b4bee5f53ccf8d
Malik Elate <Malik.e1955@gmail.com>
2023-10-12 12:31:48 -0500

    updated logo and added cards

--- frontend/src/Pages/home.js
+++ frontend/src/Pages/home.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { MainNav } from "../components/NavBar";
+import axios from "axios"; 
 
 export const Home = () => {
-
+    useEffect(() => {
+    axios.get("api/items")
+      .then((response) => {
+       console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+     }, []);
     return (
         <>
             <MainNav />
