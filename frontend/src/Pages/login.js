a3ebd80f0229ee6a78419f9dbc60ff7a74477e6e
Malik Elate <Malik.e1955@gmail.com>
2023-10-12 09:28:57 -0500

    make login center

--- frontend/src/Pages/login.js
+++ frontend/src/Pages/login.js
@@ -1,69 +1,49 @@
 import { Button } from "@/components/ui/button";
 import * as React from "react"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-    // Make the Axios GET request
-    //  axios.get(apiUrl)
-    //    .then((response) => {
-    //      // Handle the successful response here
-    //     console.log(response.data);
-    //    })
-    //    .catch((error) => {
-    //      // Handle any errors here
-    //      console.error('Error fetching data:', error);
-    //    });
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
-  return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
-    </Card>
-  )
+
+    const navigate = useNavigate();
+    const routeChange = () => {
+        navigate("/");
+    }
+
+    return (
+        <div class="min-h-screen flex items-center justify-center">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Log in</CardTitle>
+                    <CardDescription>Don't have an account? Sign up here</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Usename</Label>
+                                <Input id="username" placeholder="Name of your project" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Password</Label>
+                                <Input id="password" placeholder="Name of your project" />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex justify-center">
+                    <Button variant="outline" onClick={() => routeChange()}>Login</Button>
+                </CardFooter>
+            </Card>
+        </div>
+    )
 }
