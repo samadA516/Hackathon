2d6c43fd12f70e5c7df640b9c2efc8542f2fc81d
Alice Smith <alice@smith.com>
2023-10-12 13:51:44 -0400

    cart starter code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import { Login } from './Pages/login';
 import { SignUp } from './Pages/signup';
 import { Home } from './Pages/home';
+import { Cart } from './Pages/cart';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/login" Component={Login}/> 
-        <Route path="/signup" Component={SignUp}/> 
-        <Route path="/" Component={Home}/>
-      </Routes>
+    <Routes>
+      <Route path="/login" Component={Login} />
+      <Route path="/signup" Component={SignUp} />
+      <Route path="/cart" Component={Cart} />
+      <Route path="/" Component={Home} />
+    </Routes>
   )
 }
 
--- frontend/src/Pages/cart.js
+++ frontend/src/Pages/cart.js
@@ -1,21 +1,31 @@
 import { useEffect } from "react";
 import { MainNav } from "../components/NavBar";
-import axios from "axios"; 
+import { Table, TableBody, TableCell } from "@/components/ui/table";
+import axios from "axios";
 
 export const Cart = () => {
-    useEffect(() => {
+  useEffect(() => {
     axios.get("api/items")
       .then((response) => {
-       console.log(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-     }, []);
-    return (
-        <>
-            <MainNav />
-            cart
-        </>
-    )
+  }, []);
+  const values = [
+    { name: "asdf" }, { name: "asdf" }
+  ]
+  return (
+    <>
+      <MainNav />
+      <Table>
+        <TableBody>
+          {values.map((item) => <TableCell>item.name</TableCell>)}
+
+        </TableBody>
+      </Table>
+      cart
+    </>
+  )
 }
