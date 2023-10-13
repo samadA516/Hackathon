a25af1757c4f1428c0973167f14f3fa05d2e5ce1
Malik Elate <Malik.e1955@gmail.com>
2023-10-13 08:09:48 -0500

    dialog

--- frontend/src/Pages/cart.js
+++ frontend/src/Pages/cart.js
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
 import { MainNav } from "../components/NavBar";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardHeader,
   CardTitle,
+  CardDescription
 } from "@/components/ui/card"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogClose,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
@@ -14,67 +25,112 @@ import "../App.css";
 
 export const Cart = () => {
   const [items, setItems] = useState([]);
-  
+
   const navigate = useNavigate();
   const routeChange = () => {
-      navigate("/");
+    navigate("/");
   }
 
   useEffect(() => {
     axios.get("api/Cart")
-    .then((response) => {
-      console.log(response.data);
-      setItems(response.data);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
+      .then((response) => {
+        console.log(response.data);
+        setItems(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
       });
   }, []);
-  
+
   return (
     <>
-      <MainNav  />
-      <div className="cartContainer "> 
-      <Card className="w-[800px] theme-color">
-      <CardHeader>
-        <CardTitle>My shopping cart</CardTitle>
-        </CardHeader>
-      <Table>
-        <TableBody>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell>Product Name</TableCell>
-            <TableCell>Price</TableCell>
-            <TableCell>Rating (out of 5)</TableCell>
-            <TableCell>Fanciness (out of 5)</TableCell>
-          </TableRow>
-          {items?.map((item, index) => <TableRow key={index}> 
-            <TableCell><img src="./cow.png" width='60px' alt="issa picture"/></TableCell>
-            <TableCell>{item.item}</TableCell>
-            <TableCell>${item.price}</TableCell>
-            <TableCell>{item.rating}</TableCell>
-            <TableCell>{item.fancy}</TableCell>
-          </TableRow>)}
-          </TableBody>
-      </Table>
-      </Card>
-      <Card className = "w-[300px]">
-      <CardHeader>
-        <CardTitle>Related Items</CardTitle>
-        </CardHeader>
-      <Table>
-        <TableBody>
-           {items.map((item, index) => (<TableRow>
-            <TableCell>{item.item}</TableCell>         
-            <TableCell>{item.rating}</TableCell>
-          </TableRow>))} 
-          </TableBody>
-      </Table>
-      </Card>
-      </div>
-      <div className = "cart-button-group">
-      <Button className = "buttonSpaced">Checkout</Button>
-      <Button className = "buttonSpaced" onClick={() => routeChange()}>Keep Shopping</Button>
+      <MainNav />
+      <div className="m-5">
+        <div className="cartContainer">
+          <Card className="w-[800px] theme-color">
+            <CardHeader>
+              <CardTitle>My shopping cart</CardTitle>
+            </CardHeader>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell>Product Name</TableCell>
+                  <TableCell>Price</TableCell>
+                  <TableCell>Rating (out of 5)</TableCell>
+                  <TableCell>Fanciness (out of 5)</TableCell>
+                </TableRow>
+                {items?.map((item, index) => <TableRow key={index}>
+                  <TableCell><img src="./cow.png" width='60px' alt="issa picture" /></TableCell>
+                  <TableCell>{item.item}</TableCell>
+                  <TableCell>${item.price.toString()[0]}.{item.price?.toString()[1]}{item.price?.toString()[2]}
+                  </TableCell>
+                  <TableCell>{item.rating}</TableCell>
+                  <TableCell>{item.fancy}</TableCell>
+                </TableRow>)}
+              </TableBody>
+            </Table>
+          </Card>
+          <Card className="w-[300px]">
+            <CardHeader>
+              <CardTitle>Related Items</CardTitle>
+            </CardHeader>
+            <Table>
+              <TableBody>
+                {items.map((item, index) => (<TableRow>
+                  <TableCell>{item.item}</TableCell>
+                  <TableCell>{item.rating}</TableCell>
+                </TableRow>))}
+              </TableBody>
+            </Table>
+          </Card>
+        </div>
+        {
+          items.length === 0 ? <p className="leading-7 [&:not(:first-child)]:mt-6 text-center font-semibold">Your cart is empty</p> : null
+        } 
+        <div className="cart-button-group">
+          {
+             items.length != 0 && 
+              <Button className="buttonSpaced" style={{ marginLeft: "0px", marginRight: "0px" }} onClick={() => routeChange()} variant="outline">Keep Shopping</Button>
+          }
+          { items.length != 0 && 
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button >Checkout</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogTitle>Payment Method</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-left">
+                    Name
+                  </Label>
+                  <Input
+                    id="name"
+                    defaultValue="Pedro Duarte"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="username" className="text-left">
+                    Address
+                  </Label>
+                  <Input
+                    id="username"
+                    defaultValue="@peduarte"
+                    className="col-span-3"
+                  />
+                </div>
+              </div>
+              <DialogClose asChild>
+                <Button onClick={()=> setItems([])}type="submit">Checkout</Button>
+              </DialogClose>
+            </DialogContent>
+          </Dialog>}
+        </div>
       </div>
     </>
   );
--- frontend/src/Pages/home.js
+++ frontend/src/Pages/home.js
@@ -20,12 +20,15 @@ export const Home = () => {
         <>
             <MainNav />
             <div className="m-5">
-                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-5">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-2">
                     The Milk Road
                 </h1>
+                <p className="text-xl text-muted-foreground" style={{ width: "400px" }}>
+                    Cow's Your Chance: Savor the Best in Dairy, Without Leaving Home!
+                </p>
                 <Input
                     placeholder="Search for a product"
-                    className="w-1/2"
+                    className="w-1/2 mt-5"
                 />
                 <div className="displa-block mt-5">
                     <div className="flex display-flex flex-row gap-4 flex-wrap">
