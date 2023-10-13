10593e65c7627a65deebd43c973ffd92cdd27fc3
Sasuke Uchiha <Sasuke.Uchiha@shinobi.com>
2023-10-12 20:21:57 -0400

    changed the sign in page

--- frontend/src/Pages/signup.js
+++ frontend/src/Pages/signup.js
@@ -1,5 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+
+} from "@/components/ui/card";
 
 export const SignUp = () => {
     // Make the Axios GET request
@@ -14,10 +24,32 @@ export const SignUp = () => {
     //    });
 
     return (
-        <>
-            <Input />
-            <Input />
-            <Button>Sign up</Button>
+        <> <div class="min-h-screen flex items-center justify-center theme-color">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Sign Up</CardTitle>
+                    <CardDescription>Already have an account? Click here</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="flex flex-col space-y-1.5">
+                        <Label htmlFor="name">Username</Label>
+                        <Input id="username" placeholder="Username" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5 mt-2">
+                        <Label htmlFor="name">Password</Label>
+                        <Input type="password" id="password" placeholder="Password" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5 mt-2">
+                        <Label htmlFor="name">Confirm Password</Label>
+                        <Input type="password" id="password" placeholder="Password" />
+                    </div>
+
+                </CardContent>
+                <CardFooter className="flex justify-center">
+                    <Button>Sign up</Button>
+                </CardFooter>
+            </Card>
+        </div>
         </>
     )
 }
